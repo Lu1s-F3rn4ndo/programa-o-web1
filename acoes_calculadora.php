8a8184484739b1b3f9660a40538a26928094d0c5
Lu1s-F3rn4ndo <109108719+Lu1s-F3rn4ndo@users.noreply.github.com>
2022-08-25 19:54:57 -0300

    Add files via upload

---
+++ acoes_calculadora.php
@@ -0,0 +1,43 @@
+<?php 
+
+	$num           = "";
+	$cookie_nome1  = "num";
+	$cookie_valor1 = "";
+	$cookie_nome2  = "op";
+	$cookie_valor2 = "";
+
+	$num = isset($_POST['display']) ? $_POST['display'] : '';
+	$num = isset($_POST['submit'])  ? $_POST['display'] . $_POST['submit'] : '';
+
+	if (isset($_POST['op'])) {
+
+		$cookie_valor1 = $_POST['display'];
+		setcookie($cookie_nome1, $cookie_valor1);
+
+		$cookie_valor2 = $_POST['op'];
+		setcookie($cookie_nome2, $cookie_valor2);
+
+		$num = "";
+	}
+
+	if (isset($_POST['igual'])) {
+		$num = $_POST['display'];
+
+		switch ($_COOKIE["op"]) {
+			case "+":
+				$resultado = $num + $_COOKIE['num'];
+				break;
+			case "/":
+				$valor     = ($num / $_COOKIE['num']);
+                                $resultado = intval($valor);
+				break;
+			case "-":
+				$resultado =  $_COOKIE['num'] - $num;
+				break;
+			case "x":
+				$resultado = $num * $_COOKIE['num'];
+				break;	
+		}
+		$num = $resultado;
+	}
+?>
